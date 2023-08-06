7617c3ea686fd5dc260225f578d3f1801103b27d
Aasim <86460933+MohammadAasim@users.noreply.github.com>
2023-08-07 04:03:54 +0530

    Update 20_AccessingAndModifyingStrings.swift

---
+++ 20_AccessingAndModifyingStrings.swift
@@ -0,0 +1,12 @@
+// Accessing and Modifying Strings
+
+var message = "The quick brown for jumped over the lazy dogs."
+print(message[message.startIndex])
+print(message[message.index(after:message.startIndex)])
+print(message[message.index(before:message.endIndex)])
+
+
+let index = message.index(message.startIndex,offsetBy:5)
+let nextToIndex = message.index(message.startIndex,offsetBy:6)
+print(message[index])
+print(message[nextToIndex])
